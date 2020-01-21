c0741231a16467eacf681172d4660d1d243d1bbe
sanaz <sanazmarand@gmail.com>
2020-01-21 14:05:49 -0600

    finished

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var favoriteNumber = 21;
+
+            Console.WriteLine("Try to guess my favorite number");
+
+            var guess = int.Parse(Console.ReadLine());
+
+            if (guess < favoriteNumber)
+            {
+                Console.WriteLine("too low");
+            }
+            else if (guess > favoriteNumber)
+            {
+                Console.WriteLine("too high");
+            }
+            else
+            {
+                Console.WriteLine("correct");
+            }
+
+            var team = Console.ReadLine();
+
+            switch (team)
+            {
+                case "chiefs":
+                    Console.WriteLine("The chiefs are in the superbowl");
+                    break;
+                case "patriots":
+                    Console.WriteLine("The patriots are not in the superbowl");
+                    break;
+                default:
+                    Console.WriteLine($"The {team} and 49er's are in the superbowl");
+                    break;
+              
+            }
+            int teamName = 3;
+            switch (teamName)
+            {
+                case 0:
+                    Console.WriteLine("Seahawks");
+                    break;
+                case 1:
+                    Console.WriteLine("Chiefs");
+                    break;
+                case 2:
+                    Console.WriteLine("Texans");
+                    break;
+                case 3:
+                    Console.WriteLine("49er's");
+                    break;
+                case 4:
+                    Console.WriteLine("Patriots");
+                    break;
+
+            }
+        }    
+    }
+}
